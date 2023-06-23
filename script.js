1e16eeb68315cc64eebf1dd8d804df7777ec756b
narejomansoorali <105881827+narejomansoorali@users.noreply.github.com>
2023-06-23 09:29:26 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+(function (window) {  
+    var helloSpeaker = {};
+    var speakWord = "Hello";
+    
+    helloSpeaker.speak = function (names) {
+      console.log(speakWord + " " + names);
+    }
+        window.helloSpeaker = helloSpeaker;
+    })(window);
+
+    (function (window){
+        var byeSpeaker = {};
+        var speakWord = "Good Bye";
+        
+        byeSpeaker.speak = function (names) {
+          console.log(speakWord + " " + names);
+        }
+         
+         window.byeSpeaker = byeSpeaker;
+        })(window);
+(function () {
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
+    for (var i = 0; i < names.length; i++) {
+    
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } 
+      else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+    
+    })();
